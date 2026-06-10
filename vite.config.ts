89518a231b42ad2f8dc11ecc46e8fcee634ece71
ayannahumes <humes.ayanna>
2026-06-09 23:28:14 -0700

    correct base in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/ayanna-portfolio/",
+  base: "/ayanna_portfolio/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
